4c24e21d82ff18760e531ef94eadac2c2bc81a7b
qiankun <iamfuryman@sina.com>
2025-04-25 06:58:54 +0800

    链接数据库获取数据

--- app/p/[slug]/page.tsx
+++ app/p/[slug]/page.tsx
@@ -1,22 +1,46 @@
-import { Post ,PostProps } from "@/components/post";
+import { Post, PostProps } from "@/components/post";
 import Link from "next/link";
-
-export default  async function DetailPage({ params } : {params:Promise<{ post: PostProps }>}) {
-    const post = {
-      id: "1",
-      title: "Prisma is the perfect ORM for Next.js",
-      content:
-        "[Prisma](https://github.com/prisma/prisma) and Next.js go _great_ together!",
-      published: false,
+import prisma from "@/lib/prisma";
+type Params = {
+  slug: string
+}
+export default async function DetailPage({
+  params,
+}: {
+  params: Params;
+}) {
+  // const post = {
+  //   id: "1",
+  //   title: "Prisma is the perfect ORM for Next.js",
+  //   content:
+  //     "[Prisma](https://github.com/prisma/prisma) and Next.js go _great_ together!",
+  //   published: false,
+  //   author: {
+  //     name: "Nikolas Burk",
+  //     email: "burk@prisma.io",
+  //   },
+  // };
+  
+  const {slug} = await params
+  const post:PostProps = await prisma.post.findUnique({
+    where: {
+      id: String(slug)
+    },
+    include: {
       author: {
-        name: "Nikolas Burk",
-        email: "burk@prisma.io",
-      },
+        select: {name: true}
+      }
     }
+  })
+  const authorName = post.author ? post.author.name : "Unknown author";
   return (
     <div>
       <Link href="/">首页 </Link>
-      <Post post={post}></Post>
+      <div className="p-8 bg-slate-300">
+        <h2>{post.title}</h2>
+        <small>by {authorName}</small>
+        <p>{post.content}</p>
+      </div>
     </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,31 @@
 import { Post, PostProps } from "@/components/post";
+import prisma from '@/lib/prisma'
 // type Props = {
 //   feed: PostProps[];
 // };
 
-export default function Home() {
-  const feed = [
-    {
-      id: "1",
-      title: "Prisma is the perfect ORM for Next.js",
-      content:
-        "[Prisma](https://github.com/prisma/prisma) and Next.js go _great_ together!",
-      published: false,
+export default async function Home() {
+  // const feed = [
+  //   {
+  //     id: "1",
+  //     title: "Prisma is the perfect ORM for Next.js",
+  //     content:
+  //       "[Prisma](https://github.com/prisma/prisma) and Next.js go _great_ together!",
+  //     published: false,
+  //     author: {
+  //       name: "Nikolas Burk",
+  //       email: "burk@prisma.io",
+  //     },
+  //   },
+  // ];
+  const feed : PostProps[]  = await prisma.post.findMany({
+    where: {published: true},
+    include: {
       author: {
-        name: "Nikolas Burk",
-        email: "burk@prisma.io",
+        select: { name: true },
       },
-    },
-  ];
+    }
+  })
   return (
     <div>
       <h1 className="p-8">首页</h1>
