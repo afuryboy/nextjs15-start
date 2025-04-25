1d4f35d7ff54bfdbe181da7121e07d85c82ba4c0
qiankun <iamfuryman@sina.com>
2025-04-25 11:55:05 +0800

    a

--- app/p/[slug]/page.tsx
+++ app/p/[slug]/page.tsx
@@ -1,12 +1,10 @@
 import Link from "next/link";
 import prisma from "@/lib/prisma";
-type Params = {
-  slug: string
-}
+type Params = Promise<{slug: string}>
 export default async function DetailPage({
   params,
 }: {
-  params: Promise<Params>;
+  params: Params;
 }) {
   const {slug} = await params
   const post:any = await prisma.post.findUnique({
